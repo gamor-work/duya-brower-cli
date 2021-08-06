0b18baa77520e5a2ca579a741e8011598289d8c5
duya <18795905365@163.com>
2021-08-06 20:21:00 +0800

    feature: 搭建 cli 微应用

---
+++ .umirc.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  qiankun: {
+    slave: {},
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+});
